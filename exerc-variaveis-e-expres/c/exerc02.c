3d7b6f31aa4715ff01f319db3a39102a44c28cb5
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-03-11 22:30:31 -0300

    IFPE2024.1 LP1 Var&Exp Exerc02 complete

---
+++ exerc-variaveis-e-expres/c/exerc02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+  float real = 0.0;
+
+  printf("Enter a real number: ");
+  scanf("%f", &real);
+
+  printf("Your real number was %f.", real);
+
+  return 0;
+}
