ddf4a605558f101fe9e85e89046fe56ae839d6dc
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-03-11 22:25:08 -0300

    LP1 IFPE 2024.1 Var&Exp Exerc01 complete

---
+++ exerc-variaveis-e-expres/c/exerc01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+  // 1. Faça um programa que leia um número inteiro e o imprima.
+
+
+  int num = 0;
+
+  printf("Enter an integer (whole number): ");
+  scanf("%d", &num);
+
+  printf("Your number is %d.", num);
+
+  return 0;
+}
