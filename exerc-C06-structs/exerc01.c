9493e0c25afd7e066ba0201fcb4923dd352ab845
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-07-01 20:27:01 -0300

    Updated - exerc01 complete

--- exerc-C06-structs/exerc01.c
+++ exerc-C06-structs/exerc01.c
@@ -1,55 +1,68 @@
 // EXERCICIOS LINGUAGEM PROGRAMACAO 1 2024.1
 // C06 -> Structs
 // Problem -> 01
-// Finished by ?? Apr 2024
-
-// NEED OF REVISION. Done when I didn't fully understand structs yet
+// Finished by 30 Jun 2024
 
 #include <stdio.h>
 
-struct Horario {
+struct horario {
   int hora;
-  int min;
-  int seg;
+  int minutos;
+  int segundos;
 };
 
-struct Data {
+struct data {
   int dia;
   int mes;
   int ano;
 };
 
-struct Compromisso {
-  struct Data;
-  struct Horario;
-  char desc[50];
+struct compromisso {
+  struct data data;
+  struct horario horario;
+  char *descricao;
 };
 
+
 int main() {
-    
-  struct Horario horaAlmoco;
   
-  horaAlmoco.hora = 12;
-  horaAlmoco.min = 30;
-  horaAlmoco.seg = 00;
+  struct horario horarioAlmoco;
   
-  struct Data dataAlmoco;
+  horarioAlmoco.hora = 12;
+  horarioAlmoco.minutos = 30;
+  horarioAlmoco.segundos = 00;
   
-  dataAlmoco.dia = 12;
-  dataAlmoco.mes = 04;
+  printf("Horario Almoco: %.2d:%.2d:%.2d\n", horarioAlmoco.hora, horarioAlmoco.minutos, horarioAlmoco.segundos);
+  
+  struct data dataAlmoco;
+  
+  dataAlmoco.dia = 01;
+  dataAlmoco.mes = 07;
   dataAlmoco.ano = 2024;
   
-
+  printf("Horario Almoco: %.2d/%.2d/%d\n", dataAlmoco.dia, dataAlmoco.mes, dataAlmoco.ano);
   
-  printf("%d\n", horaAlmoco.hora);
-  printf("%d\n", horaAlmoco.min);
-  printf("%d\n", horaAlmoco.seg);
+  printf("-----------\n");
   
-  printf("%d\n", dataAlmoco.dia);
-  printf("%d\n", dataAlmoco.mes);
-  printf("%d\n", dataAlmoco.ano);
+  struct compromisso almoco;
+  
+  almoco.horario.hora = 12;
+  almoco.horario.minutos = 30;
+  almoco.horario.segundos = 00;
+  
+  printf("Horario Compromisso: %.2d:%.2d:%.2d\n", almoco.horario.hora, almoco.horario.minutos, almoco.horario.segundos);
+  
+  almoco.data.dia = 04;
+  almoco.data.mes = 8;
+  almoco.data.ano = 2024;
   
   
-
-  return 0;
+  printf("Horario Compromisso: %.2d/%.2d/%d\n",  almoco.data.dia,  almoco.data.mes,  almoco.data.ano);
+  
+  almoco.descricao = "Almoco marcado com Arthur Morgan, para combinar proximas tarefas";
+  
+  
+  printf("Descricao Compromisso: %s\n", almoco.descricao);
+  
+  
 }
