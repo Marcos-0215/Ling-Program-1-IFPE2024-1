f744fc3f6ea1425555168952657d2977bd22c842
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-07-12 22:04:38 -0300

    IFPE2024.1 LP1 C05-Strings exerc02 complete

---
+++ exerc-C05-strings/exerc02.c
@@ -0,0 +1,21 @@
+// EXERCICIOS LINGUAGEM PROGRAMACAO 1 2024.1
+// C05 - String
+// Problem -> 02
+// Finished by ?? ??? 2024
+
+
+#include <stdio.h>
+
+int main() {
+    
+  char str[] = "Amor";
+  int i = 0;
+  
+  while (str[i] != '\0') {    
+    i++;
+  }
+  
+  printf("%d\n", i);
+  
+  return 0;
+}
