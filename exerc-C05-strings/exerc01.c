fa773337ad03852fd80be828ff433ef4269c04e3
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-07-12 22:04:30 -0300

    IFPE2024.1 LP1 C05-Strings exerc01 complete

---
+++ exerc-C05-strings/exerc01.c
@@ -0,0 +1,18 @@
+// EXERCICIOS LINGUAGEM PROGRAMACAO 1 2024.1
+// C05 - String
+// Problem -> 01
+// Finished by ?? ??? 2024
+
+#include <stdio.h>
+
+int main() {
+    
+  char name[20];
+  
+  printf("Digite uma string: ");
+  scanf("%s", name);
+  
+  printf("\n%s", name);
+
+  return 0;
+}
