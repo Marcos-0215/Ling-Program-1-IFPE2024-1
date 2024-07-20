b95cd57830c683ebf45fd9a1a6e1a1a43134389d
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-07-19 21:53:34 -0300

    IFPE2024.1 LP1 C05-Strings exerc16 complete

---
+++ exerc-C05-strings/exerc16.c
@@ -0,0 +1,25 @@
+// EXERCICIOS LINGUAGEM PROGRAMACAO 1 2024.1
+// C05 -> String
+// Problem -> 16
+// Finished by 08 Jun 2024
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    
+  char str[101];
+  
+  printf("Digite caracteres maiusculos (ate 100):\n");
+  scanf("%s", str);
+  
+  for (int i=0; str[i]!='\0'; i++) {    
+    str[i] += 32;    
+  }
+  
+  printf("%s\n", str);
+
+  return 0;
+}
